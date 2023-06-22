c7020784c35700bb98d8bd6edec62e8081fb59f2
Derek Harnett <derek.harnett@shaw.ca>
2023-06-22 08:49:39 -0700

    update cssCat and cssMinify in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,42 @@
 const { src, dest, series } = require("gulp");
 const clean = require("gulp-clean");
 const sass = require("gulp-sass")(require("sass"));
-const sourcemaps = require("gulp-sourcemaps");
+const cat = require("gulp-concat");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
+const lib = "library.blocks";
+const common = "common.blocks";
+const dist = "bundles";
+
 cssClean = () => {
-    return src("bundles/*")
-    .pipe(src("common.blocks/*.css"))
-    .pipe(src("common.blocks/*.css.map"))
-    .pipe(clean());
+    return src(dist + "/*")
+        .pipe(src(common + "/*.css"))
+        .pipe(clean());
 }
 
 cssTranspile = () => {
-    return src("common.blocks/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass().on("error", sass.logError))
-    .pipe(sourcemaps.write("."))
-    .pipe(dest("common.blocks"))
+    return src(common + "/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(dest(common));
+}
+
+cssCat = () => {
+    return src([lib + "/root.css", lib + "/page.css", lib + "/card-wrapper.css", lib + "/attribution.css"])
+        .pipe(src([common + "/page.css", common + "/card.css", common + "/attribution.css"]))
+        .pipe(cat("styles.css"))
+        .pipe(dest(dist));
 }
 
 cssMinify = () => {
-    return src("bundles/styles.css")
-    .pipe(rename("bundles/styles.min.css"))
-    .pipe(cleanCSS())
-    .pipe(dest("."))
+    return src(dist + "/styles.css")
+        .pipe(rename(dist + "/styles.min.css"))
+        .pipe(cleanCSS())
+        .pipe(dest("."));
 }
 
 exports.cssClean = cssClean;
 exports.cssTranspile = cssTranspile;
+exports.cssCat = cssCat;
 exports.cssMinify = cssMinify;
-exports.default = series(cssClean, cssTranspile, cssMinify); 
+exports.default = series(cssClean, cssTranspile, cssCat, cssMinify); 
